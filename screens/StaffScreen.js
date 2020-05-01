e62a65e7e01cace2c5772c3b249c9257eed0e539
Tanc de G <tdeguigne@live.fr>
2020-05-01 20:08:02 +0200

    Barre de recherche onglet staff

--- screens/StaffScreen.js
+++ screens/StaffScreen.js
@@ -7,35 +7,42 @@ import {
   Text,
   TouchableOpacity,
   View,
+  FlatList,
 } from 'react-native';
+import { Icon} from 'native-base';
 import { WebBrowser } from 'expo';
 import { ListItem } from 'react-native-elements'
-
+import {SearchBar} from 'react-native-elements'
 
 
 import { MonoText } from '../components/StyledText';
 import CustomHeader from '../components/CustomHeader';
 
-const list = [
+const listeStaffeurs = [
   {
-    title: 'Tancrède de Guigné',
-    icon: 'person'
+    "staffeur": 'Tancrède de Guigné',
+    "icon": 'person',
+    "idStaffeur" : '1'
   },
   {
-    title: 'Saad Chtouki',
-    icon: 'person'
+    "staffeur": 'Saad Chtouki',
+    "icon": 'person',
+    "idStaffeur" : '2'
   },
   {
-    title: 'Corentin Delloye',
-    icon: 'person'
+    "staffeur": 'Corentin Delloye',
+    "icon": 'person',
+    "idStaffeur" : '3'
   },
   {
-    title: 'Pascal Raillé',
-    icon: 'person'
+    "staffeur": 'Pascal Raillé',
+    "icon": 'person',
+    "idStaffeur" : '4'
   },
   {
-    title: 'Tanguy Houette',
-    icon: 'person'
+    "staffeur": 'Tanguy Houette',
+    "icon": 'person',
+    "idStaffeur" : '5'
   },
 ]
 
@@ -44,21 +51,122 @@ class StaffScreen extends React.Component {
     header: null,
   };
 
+
+
   constructor(props) {
-        super(props);
+        super(props)
+        this.state={
+          valueSearch:'',
+          listeStaffeurs:listeStaffeurs,
+          listeStaffeursRecherches:[],
+          onResearch:false,
+          recherchesRecentes:['','','']
+        }
+        this.listeStaffeurs=listeStaffeurs
+          }
+
+
+
+    triDonnes(){
+      var text=this.state.valueSearch.toUpperCase()
+      var tableau = this.state.listeStaffeurs.filter(function(staffeur){
+        staffeurNom=staffeur.staffeur.toUpperCase()
+        return((staffeurNom.indexOf(text)!=-1)&&(staffeurNom!='NOM'))})
+      this.setState({listeStaffeursRecherches:tableau})
+
+
     }
 
-  render() {
-    return (
-      <View style={{flex:1}} >
-        <ScrollView>
+    changeText(text){
+      if (text.length==0){
+        this.setState({onResearch:false})
+      }
+      else{
+        this.setState({valueSearch:text,onResearch:true},()=>this.triDonnes())
+      }
+    }
 
+    pressStaffeur(staffeur,idStaffeur){
+
+      this.props.navigation.navigate("PdfScreen")
+
+
+    }
+
+    displayStaffeurs(){
+
+      if (this.state.onResearch){
+        return(
+          <View style={{flex:1}}>
+              <FlatList
+                data={this.state.listeStaffeursRecherches}
+                keyExtractor={(item)=>item.staffeur.toString()}
+                renderItem={({item})=>
+                <TouchableOpacity style={{height:50,justifyContent:'center'}}
+                onPress={()=>this.pressStaffeur(item.staffeur,item.idStaffeur)}>
+                  <View style={{height:50,marginLeft:10,borderColor:'#d6d6d6',borderBottomWidth:0.8,justifyContent:'center',flexDirection:'row'}}>
+                    <View style={{flex:8,justifyContent:'center'}}>
+                      <Text style={{fontWeight:'bold'}}>{item.staffeur}</Text>
+                    </View>
+                    <View style={{flex:1,justifyContent:'center',alignItems:'center'}}>
+                      <Icon style={{fontSize:20}} name='person'/>
+                    </View>
+                  </View>
+                </TouchableOpacity>
+              }
+              />
+          </View>
+
+        )
+      }
+      if ((!this.state.onResearch)&&!((this.state.recherchesRecentes[0].length>0)||(this.state.recherchesRecentes[1].length>0)||(this.state.recherchesRecentes[2].length>0))){
+        return(
+          <View style={{flex:1}}>
+              <FlatList
+                data={listeStaffeurs}
+                keyExtractor={(item)=>item.staffeur.toString()}
+                renderItem={({item})=>
+                <TouchableOpacity style={{height:50,justifyContent:'center'}}
+                onPress={()=>this.pressStaffeur(item.staffeur,item.idStaffeur)}>
+                  <View style={{height:50,marginLeft:10,borderColor:'#d6d6d6',borderBottomWidth:0.8,justifyContent:'center',flexDirection:'row'}}>
+                    <View style={{flex:8,justifyContent:'center'}}>
+                      <Text style={{fontWeight:'bold'}}>{item.staffeur}</Text>
+                    </View>
+                    <View style={{flex:1,justifyContent:'center',alignItems:'center'}}>
+                      <Icon style={{fontSize:20}} name='person'/>
+                    </View>
+                  </View>
+                </TouchableOpacity>
+              }
+              />
+          </View>
+
+        )
+      }
+    }
+
+
+  render() {
+    const {search} = this.state;
+    return (
+
+
+/*      <View style={{flex:1}} >
+        <ScrollView>
         <CustomHeader title="Staffeurs" isHome={true} navigation={this.props.navigation}/>
+        <SearchBar
+        platform="ios"
+        cancelButtonTitle="Cancel"
+        placeholder='Rechercher'
+        onClearText={()=>
+      this.setState({onResearch:false,valueSearch:''})}
+        onChangeText={(text)=>this.changeText(text)}
+        value={this.state.valueSearch}/>
             {
-              list.map((item, i) => (
+              listeStaffeurs.map((item, i) => (
                 <ListItem
                   key={i}
-                  title={item.title}
+                  title={item.staffeur}
                   leftIcon={{ name: item.icon }}
                   bottomDivider
                   chevron
@@ -68,6 +176,20 @@ class StaffScreen extends React.Component {
             }
         </ScrollView>
 
+      </View>*/
+      <View style={{flex:1}} >
+        <SearchBar
+          platform="ios"
+          cancelButtonTitle="Cancel"
+          placeholder='Rechercher'
+          onClearText={()=>
+        this.setState({onResearch:false,valueSearch:''})}
+          onChangeText={(text)=>this.changeText(text)}
+          value={this.state.valueSearch}/>
+        <ScrollView>
+
+        {this.displayStaffeurs()}
+        </ScrollView>
       </View>
     );
   }
